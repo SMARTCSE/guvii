1b9f49f139e42e7c9fa7366b703f401caeaaaac2
SMARTCSE <47376568+SMARTCSE@users.noreply.github.com>
2019-03-08 02:33:40 -0800

    Create leapyear.java

---
+++ leapyear.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Exercise18 {
+   public static void main(String[] args)
+   Class Ideone
+   //Main
+    {
+    int year = 1998;
+       System.out.println();        
+       if((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0)))
+               System.out.println("Year " + year + "Leap year");
+       else
+               System.out.println("Year " + year + "Not a Leap year");
+               System.out.println();
+    }
+}
